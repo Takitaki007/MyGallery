2012f3963c63867bb3ea9de8790178991410a0ba
Tikea <teekeaouch745@gmail.com>
2025-06-04 10:01:17 +0700

    update profile

--- app/(root)/profile/page.jsx
+++ app/(root)/profile/page.jsx
@@ -3,11 +3,13 @@ import { LogoutCurve, User } from "iconsax-reactjs";
 export default function ProfilePage() {
   return (
     <main>
-      <section className="grid grid-cols-12 bg-red-300 px-90">
+    
+      <section className="grid grid-cols-12  px-90">
         {/* Header */}
-        <div className="col-span-12 bg-blue-300 flex items-center justify-between p-5">
-          <h1 className="font-semibold text-lg text-white">Personal Information</h1>
-          <LogoutCurve size="24" variant="Broken" color="#ffffff" />
+
+        <div className="col-span-12 bg-gray-300 flex items-center justify-between p-3 rounded-lg">
+          <h1 className="font-semibold text-lg ">Personal Information</h1>
+          <LogoutCurve size="24" variant="Broken" color="#94A3B8" />
         </div>
 
         {/* Profile Image Section */}
@@ -21,38 +23,41 @@ export default function ProfilePage() {
         </div>
         <form className="flex items-center gap-5">
             <div>
-            <label for="fullname" className="flex items-center gap-2 mt-5">
+            <label htmlFor="fullname" className="flex items-center gap-2 mt-5">
                 <User size="24" variant="Broken" color="#000000"/>
                 Full Name
             </label>
-            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-yellow-400 p-2 rounded-lg mt-2 w-[400px]"></input>
-                        <label for="fullname" className="flex items-center gap-2 mt-5">
+            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-gray-200 p-2 rounded-lg mt-2 w-[430px]"></input>
+                        <label htmlFor="fullname" className="flex items-center gap-2 mt-5">
                 <User size="24" variant="Broken" color="#000000"/>
                 Full Name
             </label>
-            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-yellow-400 p-2 rounded-lg mt-2 w-[400px]"></input>
+            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-gray-200 p-2 rounded-lg mt-2 w-[430px]"></input>
                 </div>
                    <div>
-            <label for="fullname" className="flex items-center gap-2 mt-5">
+            <label htmlFor="fullname" className="flex items-center gap-2 mt-5">
                 <User size="24" variant="Broken" color="#000000"/>
                 Full Name
             </label>
-            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-yellow-400 p-2 rounded-lg mt-2 w-[400px]"></input>
-                        <label for="fullname" className="flex items-center gap-2 mt-5">
+            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-gray-200 p-2 rounded-lg mt-2 w-[430px]"></input>
+                        <label htmlFor="fullname" className="flex items-center gap-2 mt-5">
                 <User size="24" variant="Broken" color="#000000"/>
                 Full Name
             </label>
-            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-yellow-400 p-2 rounded-lg mt-2 w-[400px]"></input>
+            <input type="text" id="fullname" placeholder="Enter Your Full Name" className="bg-gray-200 p-2 rounded-lg mt-2 w-[430px]"></input>
             
                 </div>
 
         </form>
      <div className="col-span-12 mt-5 flex justify-end">
-         <button type="submit" className="bg-blue-500 text-white font-bold rounded-lg p-2 w-[100px]">
+         <button type="submit" className="bg-gray-800 text-white font-bold rounded-lg p-2 w-[100px]">
         Edit
       </button>
      </div>
+
       </section>
+
+
     </main>
   );
 }
